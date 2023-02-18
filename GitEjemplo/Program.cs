0eb1ddc0a8de1a6e2cc75e2dac1720fa670550a7
Drew <Drew@DESKTOP-ARG8DKT>
2023-02-18 01:01:10 -0700

    Agregar archivos de proyecto.

---
+++ GitEjemplo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitEjemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mensaje subido en GitHub");
+        }
+    }
+}
