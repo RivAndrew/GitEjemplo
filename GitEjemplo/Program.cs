8354a198fadd46b9450033cb375b1aa5fc7b1b64
RivAndrew <andrew_rivera1000@hotmail.com>
2023-02-18 01:03:52 -0700

    Se agrego un cambio para probar el commit desde Visual Studio

--- GitEjemplo/Program.cs
+++ GitEjemplo/Program.cs
@@ -11,6 +11,7 @@ namespace GitEjemplo
         static void Main(string[] args)
         {
             Console.WriteLine("Mensaje subido en GitHub");
+            Console.WriteLine("Mensaje ejemplo para subir un commit desde Visual Studio");
         }
     }
 }
